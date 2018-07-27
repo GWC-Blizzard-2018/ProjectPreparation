3579369433f779df26a2da8a3e41ff6437a0143a
Hailey Park <haileyswpa@gmail.com>
2018-07-27 10:55:15 -0700

    fixed all the links

--- TriviaGame/assets/js/app.js
+++ TriviaGame/assets/js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-  
+
   // event listeners
   $("#remaining-time").hide();
   $("#start").on('click', trivia.startGame);
   $(document).on('click' , '.option', trivia.guessChecker);
-  
+
 })
 
 var trivia = {
@@ -18,30 +18,30 @@ var trivia = {
   timerId : '',
   // questions options and answers data
   questions: {
-    q1: 'Who is actually a chef?',
-    q2: 'What does Joey love to eat?',
-    q3: 'How many times has Ross been divorced?',
-    q4: 'How many types of towels does Monica have?',
-    q5: "Who stole Monica's thunder after she got engaged?",
-    q6: 'Who hates Thanksgiving?',
+    q1: 'What is one of the benefits of going to college out-of-state?',
+    q2: 'Which of the following is not true about the Work-Study Program?',
+    q3: 'Which type of schooling is normally cheaper?',
+    q4: 'How is a Private School different from a Public school?',
+    q5: "Normally, how many years is Community College?",
+    q6: 'What is a loan?',
     q7: "Who thinks they're always the last to find out everything?"
   },
   options: {
-    q1: ['Monica', 'Chandler', 'Rachel', 'Ross'],
-    q2: ['Fish', 'Apples', 'Oranges', 'Sandwhiches'],
-    q3: ['5', '2', '1', '3'],
-    q4: ['3', '8', '11', '6'],
-    q5: ['Rachel','Phoebe','Emily','Carol'],
-    q6: ['Joey','Chandler','Rachel','Ross'],
+    q1: ['Cheaper', 'Not as intimidating', 'Experience new lifestyles', 'Easier to get into'],
+    q2: ['Available to full-time students', 'Available only for undergraduates', 'Only administered by schools participating in the Federal Work-Study Program', 'Available to part-time students'],
+    q3: ['University', 'Community College', 'Both are cheap', 'Both are expensive'],
+    q4: ['Private schools are more expensive', 'Private schools are often less inclusive', 'Private schools have more clubs and societies', 'Private schools have a larger acceptance rate'],
+    q5: ['2 years','4 year','3 years','However long it takes'],
+    q6: ['Financial aid that doesn’t have to be repaid','Aid for military service or for being the spouse or child of a veteran','Vouchers for current and former foster care youth','Borrowed money for school that must be repaid, with interest'],
     q7: ['Ross', 'Phoebe', 'Monica','Chandler']
   },
   answers: {
-    q1: 'Monica',
-    q2: 'Sandwhiches',
-    q3: '3',
-    q4: '11',
-    q5: 'Rachel',
-    q6: 'Chandler',
+    q1: 'Experience new lifestyles',
+    q2: 'Available only for undergraduates',
+    q3: 'Community College',
+    q4: 'Private schools are more expensive',
+    q5: '2 years',
+    q6: 'Borrowed money for school that must be repaid, with interest',
     q7: 'Phoebe'
   },
   // trivia methods
@@ -53,50 +53,50 @@ var trivia = {
     trivia.incorrect = 0;
     trivia.unanswered = 0;
     clearInterval(trivia.timerId);
-    
+
     // show game section
     $('#game').show();
-    
+
     //  empty last results
     $('#results').html('');
-    
+
     // show timer
     $('#timer').text(trivia.timer);
-    
+
     // remove start button
     $('#start').hide();
 
     $('#remaining-time').show();
-    
+
     // ask first question
     trivia.nextQuestion();
-    
+
   },
-  // method to loop through and display questions and options 
+  // method to loop through and display questions and options
   nextQuestion : function(){
-    
+
     // set timer to 20 seconds each question
-    trivia.timer = 10;
+    trivia.timer = 15;
      $('#timer').removeClass('last-seconds');
     $('#timer').text(trivia.timer);
-    
+
     // to prevent timer speed up
     if(!trivia.timerOn){
       trivia.timerId = setInterval(trivia.timerRunning, 1000);
     }
-    
+
     // gets all the questions then indexes the current questions
     var questionContent = Object.values(trivia.questions)[trivia.currentSet];
     $('#question').text(questionContent);
-    
+
     // an array of all the user options for the current question
     var questionOptions = Object.values(trivia.options)[trivia.currentSet];
-    
+
     // creates all the trivia guess options in the html
     $.each(questionOptions, function(index, key){
       $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
     })
-    
+
   },
   // method to decrement counter and count unanswered if timer runs out
   timerRunning : function(){
@@ -118,7 +118,7 @@ var trivia = {
     }
     // if all the questions have been shown end the game, show results
     else if(trivia.currentSet === Object.keys(trivia.questions).length){
-      
+
       // adds results of game (correct, incorrect, unanswered) to the page
       $('#results')
         .html('<h3>Thank you for playing!</h3>'+
@@ -126,29 +126,29 @@ var trivia = {
         '<p>Incorrect: '+ trivia.incorrect +'</p>'+
         '<p>Unaswered: '+ trivia.unanswered +'</p>'+
         '<p>Please play again!</p>');
-      
+
       // hide game sction
       $('#game').hide();
-      
+
       // show start button to begin a new game
       $('#start').show();
     }
-    
+
   },
   // method to evaluate the option clicked
   guessChecker : function() {
-    
+
     // timer ID for gameResult setTimeout
     var resultId;
-    
+
     // the answer to the current question being asked
     var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
-    
+
     // if the text of the option picked matches the answer of the current question, increment correct
     if($(this).text() === currentAnswer){
       // turn button green for correct
       $(this).addClass('btn-success').removeClass('btn-info');
-      
+
       trivia.correct++;
       clearInterval(trivia.timerId);
       resultId = setTimeout(trivia.guessResult, 1000);
@@ -158,27 +158,27 @@ var trivia = {
     else{
       // turn button clicked red for incorrect
       $(this).addClass('btn-danger').removeClass('btn-info');
-      
+
       trivia.incorrect++;
       clearInterval(trivia.timerId);
       resultId = setTimeout(trivia.guessResult, 1000);
       $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
     }
-    
+
   },
   // method to remove previous question results and options
   guessResult : function(){
-    
+
     // increment to next question set
     trivia.currentSet++;
-    
+
     // remove the options and results
     $('.option').remove();
     $('#results h3').remove();
-    
+
     // begin next question
     trivia.nextQuestion();
-     
+
   }
 
 }
